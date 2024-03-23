84da3fe2b3d5584d1e1f34dbbe305789f418f720
dark-king-001 <piyushat115@gmail.com>
2024-03-23 12:22:46 +0530

    added crud for collections

--- backend-code/routes/collectionRoutes.js
+++ backend-code/routes/collectionRoutes.js
@@ -3,10 +3,15 @@ const router = express.Router();
 
 const collectionController = require("../controllers/collectionController");
 
+// done
 router.get("/collections", collectionController.getAllCollections);
+// done
 router.post("/collections", collectionController.createCollection);
 router.get("/collections/:id", collectionController.getCollectionById);
+
+// done
 router.put("/collections/:id", collectionController.updateCollection);
+// done
 router.delete("/collections/:id", collectionController.deleteCollection);
 
 module.exports = router;
